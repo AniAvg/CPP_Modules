c313e04106d04dc97e471a0d007a1da37f1dc691
Ani <aniavg2003@gmail.com>
2026-03-29 16:25:20 +0400

    started Account.cpp implementation

--- CPP_Module00/ex02/Account.cpp
+++ CPP_Module00/ex02/Account.cpp
@@ -1,3 +1,39 @@
 #include "Account.hpp"
 
+int	Account::getNbAccounts(void)
+{
+	return (_nbAccounts);
+}
 
+int	Account::getTotalAmount(void)
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits(void)
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return (_totalNbWithdrawals);
+}
+
+
+void	Account::displayAccountsInfos(void)
+{
+	std::cout << "Accounts: " << getNbAccounts() << std::endl;
+	std::cout << "Amount: " << getTotalAmount() << std::endl;
+	std::cout << "Deposits: " << getNbDeposits() << std::endl;
+	std::cout << "Withdrawals: " << getNbWithdrawals() << std::endl;
+}
+
+
+// constructor
+Account::Account(int initial_deposit)
+{}
+
+// destructor
+Account::~Account(void)
+{}
