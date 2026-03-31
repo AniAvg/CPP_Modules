e30173927b3e9abf500effb3d4127301bbf2e6b3
Ani Avagyan <anavagya@student.42yerevan.am>
2026-03-31 12:07:49 +0400

    finished cpp00/ex02

--- CPP_Module00/ex02/Account.cpp
+++ CPP_Module00/ex02/Account.cpp
@@ -2,10 +2,12 @@
 #include <iostream>
 #include <ctime>
 
+
 int	Account::_nbAccounts = 0;
 int	Account::_totalAmount = 0;
 int	Account::_totalNbDeposits = 0 ;
 int	Account::_totalNbWithdrawals = 0;
+
 
 int	Account::getNbAccounts(void)
 {
