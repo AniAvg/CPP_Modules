2b723684441bafd78fadccbaa17a75a80f3a28aa
Ani <aniavg2003@gmail.com>
2026-04-04 19:17:35 +0400

    + ex05

---
+++ CPP_Module01/ex05/main.cpp
@@ -0,0 +1,15 @@
+#include "Harl.hpp"
+
+int	main()
+{
+	Harl	harl;
+
+	harl.complain("BLABLA");
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+
+	return (0);
+}
