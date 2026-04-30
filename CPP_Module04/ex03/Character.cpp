340757479f0d5909f484563a2b5903abc741ff75
Ani Avagyan <anavagya@student.42yerevan.am>
2026-04-30 18:24:35 +0400

    add Character

---
+++ CPP_Module04/ex03/Character.cpp
@@ -0,0 +1,11 @@
+#include "Character.hpp"
+
+Character::Character()
+{
+	std::cout << "Character default constructor called" << std::endl;
+}
+
+Character::~Character()
+{
+	std::cout << "Character destructor called" << std::endl;
+}
