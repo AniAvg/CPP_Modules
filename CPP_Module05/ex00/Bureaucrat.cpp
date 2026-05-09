70368bafdab65505213e0602ab5b398763360e42
Ani Avagyan <anavagya@student.42yerevan.am>
2026-05-09 17:17:41 +0400

    add

--- CPP_Module05/ex00/Bureaucrat.cpp
+++ CPP_Module05/ex00/Bureaucrat.cpp
@@ -1,5 +1,21 @@
 #include "Bureaucrat.hpp"
 
+Bureaucrat::Bureaucrat() : name("default"), grade(150)
+{}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other) : name(other.name), grade(other.grade)
+{}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{
+	if (this != &other)
+		this->grade = other.grade;
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{}
+
 Bureaucrat::Bureaucrat(const std::string& name, int grade) : name(name), grade(grade)
 {
 	if (this->grade < 1)
@@ -9,29 +25,34 @@ Bureaucrat::Bureaucrat(const std::string& name, int grade) : name(name), grade(g
 }
 
 
-std::string Bureaucrat::getName()
+std::string Bureaucrat::getName() const
 {
 	return this->name;
 }
 
-int Bureaucrat::getGrade()
+int Bureaucrat::getGrade() const
 {
 	return this->grade;
 }
 
 void Bureaucrat::increment_grade()
 {
-	if (this->grade == 1)
-		this->grade = 150;
-	else
-		this->grade--;
+	if (this->grade <= 1)
+		throw GradeTooHighException();
+	this->grade--;
 }
 
 void Bureaucrat::decrement_grade()
 {
-	if (this->grade == 150)
-		this->grade = 150;
-	else
-		this->grade++;
+	if (this->grade >= 150)
+		throw GradeTooLowException();
+	this->grade++;
 }
 
+
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& b)
+{
+	out << b.getName() << ", bureaucrat grade " << b.getGrade();
+	return out;
+}
--- CPP_Module05/ex00/Bureaucrat.hpp
+++ CPP_Module05/ex00/Bureaucrat.hpp
@@ -11,9 +11,13 @@ private:
 	std::string const	name;
 	int		grade;
 public:
+	Bureaucrat();
+	Bureaucrat(const Bureaucrat& other);
+	Bureaucrat& operator=(const Bureaucrat& other);
+	~Bureaucrat();
 	Bureaucrat(const std::string& name, int grade);
-	std::string getName();
-	int getGrade();
+	std::string getName() const;
+	int getGrade() const;
 	void increment_grade();
 	void decrement_grade();
 
@@ -37,4 +41,8 @@ public:
 	};
 
 };
+
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& b);
+
 
